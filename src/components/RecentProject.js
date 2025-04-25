5aa14bca6bf4a38a6da11300b5e892deb5674a71
devhan-hub <hanabduman@gmail.com>
2025-04-25 23:15:42 +0300

    feat: add hover animation effect for recent projects

--- src/components/RecentProject.js
+++ src/components/RecentProject.js
@@ -1,9 +1,44 @@
+'use client'
 import { recentProject } from "@/lib/serviceDate";
-import { HoverEffect } from "./HoverCard";
+import Image from "next/image";
+import {motion} from "framer-motion";
+import { useState } from "react";
 
 const RecentProject = () => {
+    const [hoveredIndex, setHoveredIndex] = useState(null);
+    const springConfig = { type: "spring", stiffness: 100, damping: 20 };
+
+    
     return (
-          <HoverEffect items={recentProject}/>       
+       <div className="flex items-center justify-center mt-12 md:mt-20">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
+            {
+                recentProject.map((item, index) => (
+                    <motion.div
+                    key={index}
+                    className="relative p-4 rounded-lg"
+                    initial={{ scale: 1, opacity: 1, y: 0 }}
+                    animate={{
+                        scale: hoveredIndex === null ? 1 : hoveredIndex === index ? 1.02 : 0.95,
+                        opacity: hoveredIndex === null ? 1 : hoveredIndex === index ? 1 : 0.5,
+                        y: hoveredIndex === index ? -5 : 0,
+                    }}
+                    transition={springConfig}
+                    onHoverStart={() => setHoveredIndex(index)}
+                    onHoverEnd={() => setHoveredIndex(null)}                  
+                    whileHover={{
+                        boxShadow: "0 0 15px rgba(84, 194, 32, 0.3)",
+                    }}
+                >
+                            <Image width={1000} height={1000} src={item.img} alt={item.name} layout="responsive" className="rounded-lg shadow-lg object-cover w-full h-64 mb-4" />
+                        <p className="text-left mb-2 font-semibold text-xl">{item.name}</p>
+                        <p className="text-left text-foreground-500/70 line-clamp-2 text-sm">{item.desc}</p>
+                        </motion.div>
+                ))
+
+            }
+        </div>
+       </div>
     );
 };
 
