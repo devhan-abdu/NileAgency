ef0457c6aae55e0af7ede583bf00736293369ee2
devhan-hub <hanabduman@gmail.com>
2025-04-29 11:11:03 +0300

    feat: add floating nav on scroll up and fix header alignment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,93 +3,82 @@ import { cn } from "@/lib/utils";
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 import { CgMenuLeft } from "react-icons/cg";
-import { buttonVariants } from "./ui/button";
-
-
 import {
     Sheet,
     SheetContent,
     SheetTrigger,
 } from "@/components/ui/sheet"
-
+import { FloatingNav } from "./ui/FloatedNav";
 
 const Header = () => {
     const pathName = usePathname();
     const navLinks = [
         { name: 'Home', href: '/' },
         { name: 'Services', href: '/services' },
+        { name: 'Portfolio', href: '/portfolio' },
         { name: 'About Us', href: '/about' },
-        { name: 'Contact Us', href: '/contact' }
     ]
 
+
+    const NavMap = () => {
+        return (
+            navLinks.map((navLink) => {
+                const isActive = navLink.href === pathName;
+                return (
+                    <li
+                        key={navLink.href}
+                        className={cn(
+                            "hover:text-primary-500 duration-300",
+                            isActive && "text-primary-500"
+                        )}
+                    >
+                        <Link href={navLink.href}>{navLink.name}</Link>
+                    </li>
+                )
+            })
+       )
+    }
     return (
 
-        <header className='flex items-center font-montserrat py-8 text-foreground-500'>
-            <div className="flex justify-between items-center gap-4  w-full ">
-                <Link href={'/'} className=" text-primary-500  text-3xl  font-semibold ">
+        <header className='flex items-center justify-between font-poppins  text-foreground-500 px-4 py-8'>
+            <FloatingNav navItems={navLinks} />
+            <div className="flex justify-between items-center place-content-center gap-4 w-full">
+                <Link href={'/'} className="text-primary-500 text-3xl font-semibold inline-block font-montserrat">
                     NileAgency
-                </Link >
-                <nav className='hidden md:block'>
-                    <ul className='flex justify-between items-center gap-6 w-full py-1'>
+                </Link>
+                <nav className='hidden md:flex grow'>
+                    <ul className='flex justify-between items-center gap-6 mx-auto'>
                         {
-                            navLinks.map((navLink) => {
-                                const isActive = navLink.href === pathName;
-                                return (
-                                    <li
-                                        key={navLink.href}
-                                        className={cn(
-                                            " hover:text-primary-500  duration-300  ",
-                                            isActive && "text-primary-500"
-                                        )}
-                                    >
-                                        <Link href={navLink.href} >{navLink.name}</Link>
-                                    </li>)
-                            })
+                            <NavMap />
                         }
                     </ul>
+                    <Link href={'/contact'}
+                        className=" border text-sm font-medium relative border-white/[0.2] text-black bg-primary-500 px-4 py-2 rounded-lg ">
+                        <span>Become a client</span>
+                    </Link>
                 </nav>
-                <Link
-                    href="/contact"
-                    className={cn(
-                        buttonVariants({ variant: 'default' }),
-                        '   hover:scale-[1.05] transform duration-300 hidden md:block text-black font-semibold '
-                    )}
-                >
-                    Become a client
-                </Link>
-            </div>
-            <div className="flex md:hidden">
-                <Sheet className=''>
-                    <SheetTrigger className='mx-2 sm:mx-4'>
-                        < CgMenuLeft className='text-primary-500 text-3xl' />
-                    </SheetTrigger>
-                    <SheetContent className='w-[250px] bg-black/90 shadow-md shadow-primary-500  px-12 py-28'>
 
-                        <nav>
-                            <ul className='flex flex-col justify-between gap-6 text-gray-400'>
-                                {
-                                    navLinks.map((navLink) => {
-                                        const isActive = navLink.href === pathName;
-                                        return (
-                                            <li
-                                                key={navLink.href}
-                                                className={cn(
-                                                    "font-semibold hover:text-primary-500 duration-300",
-                                                    isActive && "text-primary-500"
-                                                )}
-                                            >
-                                                <Link href={navLink.href}>{navLink.name}</Link>
-                                            </li>
-                                        )
-                                    })
-                                }
-                            </ul>
-                        </nav>
-                    </SheetContent>
-                </Sheet>
             </div>
+            <Sheet className='flex md:hidden w-full px-4'>
+                <SheetTrigger className='ml-auto md:hidden'>
+                    <CgMenuLeft className='text-primary-500 text-3xl' />
+                </SheetTrigger>
+                <SheetContent className='w-[250px] bg-black/90 shadow-md shadow-foreground-500 px-12 py-28'>
+                    <nav>
+                        <ul className='flex flex-col justify-between gap-6 text-foreground-500'>
+                            {
+                                <NavMap />
+                            }
+                        </ul>
+                    </nav>
+                </SheetContent>
+            </Sheet>
         </header>
     )
 }
 
 export default Header
+
+
+
+
