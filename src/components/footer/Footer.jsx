f872a64cef932881bdc22afde6863d34405702c8
devhan-hub <hanabduman@gmail.com>
2025-05-04 13:56:15 +0300

    chore: update footer layout and content

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,92 +1,126 @@
-'use client'
+'use client';
 import { usePathname } from 'next/navigation';
+import { useEffect, useState } from 'react';
 import { cn } from '@/lib/utils';
 import Link from 'next/link';
 import React from 'react';
 import {
-    Linkedin,
-    Instagram,
-    Twitter,
-    Facebook,
-    Youtube,
+  Linkedin,
+  Instagram,
+  Twitter,
+  Facebook,
+  Youtube,
 } from 'lucide-react';
 
 const Footer = () => {
-    const pathName = usePathname();
-    const navLinks = [
-        { name: 'Home', href: '/' },
-        { name: 'Services', href: '/services' },
-        { name: 'Portfolio', href: '/portfolio' },
-        { name: 'About Us', href: '/about' },
-        { name: 'Contact Us', href: '/contact' },
-    ]
+  const pathName = usePathname();
+  const [year, setYear] = useState(null);
 
-    const socialLinks = [
-        { icon: <Linkedin />, href: 'https://linkedin.com' },
-        { icon: <Instagram />, href: 'https://instagram.com' },
-        { icon: <Twitter />, href: 'https://twitter.com' },
-        { icon: <Facebook />, href: 'https://facebook.com' },
-        { icon: <Youtube />, href: 'https://youtube.com' },
-    ];
+  useEffect(() => {
+    setYear(new Date().getFullYear());
+  }, []);
 
-    const NavMap = () => {
-        return (
-            navLinks.map((navLink) => {
-                const isActive = navLink.href === pathName;
-                return (
-                    <li
-                        key={navLink.href}
-                        className={cn(
-                            "hover:text-primary-500 duration-300",
-                            isActive && "text-primary-500"
-                        )}
-                    >
-                        <Link href={navLink.href}>{navLink.name}</Link>
-                    </li>
-                )
-            })
-        )
-    }
-    return (
-        <div className='text-foreground-500 font-poppins  mb-2  mt-16 flex items-center justify-center '>
+  const navLinks = [
+    { name: 'Home', href: '/' },
+    { name: 'Services', href: '/services' },
+    { name: 'Portfolio', href: '/portfolio' },
+    { name: 'About Us', href: '/about' },
+    { name: 'Contact Us', href: '/contact' },
+  ];
 
-            <div className='flex flex-col items-center justify-center gap-6  pb-6  bg-[#202020] px-2 rounded-lg w-full'>
-                <div className=' p-6 relative rounded-lg w-full sm:w-[80%] max-w-3xl -mt-12 z-10'>
-                    <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-6 [background:radial-gradient(125%_125%_at_50%_10%,#202020_60%,#5dd623_100%)] rounded-sm"></div>
-                    <div className='flex flex-col md:flex-row items-center justify-center gap-6 '>
-                        <p className='text-lg md:text-xl font-montserrat font-semibold'>Subscribe to our newsLetter</p>
-                        <div className="inline-flex border border-foreground-500 focus-within:border-primary-500 text-white rounded-full transition">
-                            <input
-                                type="text"
-                                className="outline-none px-2 py-2 bg-transparent text-white"
-                                placeholder="Enter email"
-                            />
-                            <button className="text-black bg-white rounded-r-full px-2 font-bold cursor-pointer">
-                                Subscribe
-                            </button>
-                        </div>
+  const socialLinks = [
+    { icon: <Linkedin />, href: 'https://linkedin.com' },
+    { icon: <Instagram />, href: 'https://instagram.com' },
+    { icon: <Twitter />, href: 'https://twitter.com' },
+    { icon: <Facebook />, href: 'https://facebook.com' },
+    { icon: <Youtube />, href: 'https://youtube.com' },
+  ];
 
+  const NavMap = () => {
+    return navLinks.map((navLink) => {
+      const isActive = navLink.href === pathName;
+      return (
+        <li
+          key={navLink.href}
+          className={cn(
+            'hover:text-primary-500 duration-300 text-[14px]',
+            isActive && 'text-primary-500'
+          )}
+        >
+          <Link href={navLink.href}>{navLink.name}</Link>
+        </li>
+      );
+    });
+  };
 
-                    </div>
-                </div>
+  return (
+    <div className="text-gray-300 font-poppins pt-12 pb-6 px-8 md:px-12 border-t border-gray-400 rounded-md mt-16 ">
+      <div className="flex flex-col md:flex-row gap-12">
+        <div className="w-full md:w-[25%] flex flex-col justify-between gap-6">
+          <div>
+            <h1 className="text-3xl font-bold font-montserrat uppercase text-foreground-500">NileAgency</h1>
+            <p className="text-[14px]">Design. Strategy. Results.</p>
+          </div>
 
-                <h1 className='text-3xl font-bold font-montserrat uppercase'>NileAgency</h1>
-                <ul className='flex flex-wrap justify-center items-center gap-6 mx-auto mt-2 '>
-                    {
-                        <NavMap />
-                    }
-                </ul>
-                <div className='flex gap-3 items-center mt-2 mb-6'>
-                    {
-                        socialLinks.map((item, index) => (
-                            <Link key={index} target='_blank' href={item.href} className='w-10 h-10 rounded-full flex items-center justify-center bg-white text-black hover:scale-105  transition duration-300'>  {item.icon}</Link>
-                        ))
-                    }
-                </div>
-                <p>Copyright &copy; <span className='font-semibold'>NileAgency</span> </p>
+          <div>
+            <div className="flex gap-3 items-center">
+              {socialLinks.map((item, index) => (
+                <Link
+                  key={index}
+                  target="_blank"
+                  href={item.href}
+                  className="w-8 h-8 rounded-full flex items-center justify-center bg-white text-black hover:scale-105 transition duration-300"
+                >
+                  {item.icon}
+                </Link>
+              ))}
             </div>
+            <h3 className="text-lg font-bold mt-2">Follow Us</h3>
+          </div>
         </div>
-    )
-}
 
-export default Footer
+        <div className="w-full md:w-[40%] flex flex-col justify-between gap-6">
+          <ul className="flex flex-wrap gap-4">{<NavMap />}</ul>
+          <div>
+            <h3 className="text-xl font-bold">About Us</h3>
+            <p className="text-[14px]">
+              We help brands grow through impactful digital strategies and stunning design.
+              Based by the Nile, driven by creativity and results.
+            </p>
+          </div>
+        </div>
+
+        <div className="w-full md:w-[35%] flex flex-col justify-between gap-6 border-t md:border-t-0 md:border-l pl-0 md:pl-8 pt-6 md:pt-0 border-gray-600">
+          <div className="space-y-4">
+            <div>
+              <h3 className="text-lg font-bold mb-1">Call:</h3>
+              <p className="ml-4 text-[14px] text-foreground-500/90">+25195234125</p>
+              <p className="ml-4 text-[14px] text-foreground-500/90">+251743567212</p>
+            </div>
+            <div>
+              <h3 className="text-lg font-bold mb-1">Email:</h3>
+              <p className="ml-4 text-[14px] text-foreground-500/90">nileagency12@gmail.com</p>
+              <p className="ml-4 text-[14px] text-foreground-500/90">nileagencyhan@gmail.com</p>
+            </div>
+          </div>
+          <div className="inline-flex  w-max mr-auto border border-foreground-500 focus-within:border-primary-500 text-white rounded-full transition">
+            <input
+              type="text"
+              className="outline-none px-2 py-2 bg-transparent text-white"
+              placeholder="Enter email"
+            />
+            <button className="text-black bg-primary-500 rounded-r-full px-3 font-bold cursor-pointer">
+              Subscribe
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="text-center mt-6  text-foreground-500">
+        Copyright &copy; {year && <span>{year}</span>}<span className="font-bold"> NileAgency</span>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
